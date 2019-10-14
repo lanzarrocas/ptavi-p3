01645685e3038a855e49d3bb02bfd6ef2a601a03
Pablo Esteban Martin <pesteban@f-l3207-pc47.aulas.etsit.urjc.es>
2019-10-14 14:06:18 +0200

    avance1

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -6,6 +6,7 @@ from xml.sax.handler import ContentHandler
 
 class SmallSMILHandler(ContentHandler):
     def __init__(self):
+        self.list = []
         self.dicc = {"root-layout": ["width", "height", "background-color"],
                      "region": ["id", "top", "bottom", "left", "right"],
                      "img": ["src", "region", "begin", "dur"],
@@ -13,20 +14,35 @@ class SmallSMILHandler(ContentHandler):
                      "textstream": ["src", "region"]}
 
     def startElement(self, name, attrs):
+        aux = {}
         if name == "root-layout":
-            pass
+            for att in self.dicc[name]:
+                aux[att] = attrs.get(att, "")
+            self.list.append([name, aux])
         elif name == "region":
-            pass
+            for att in self.dicc[name]:
+                aux[att] = attrs.get(att, "")
+            self.list.append([name, aux])
         elif name == "img":
-            pass
+            for att in self.dicc[name]:
+                aux[att] = attrs.get(att, "")
+            self.list.append([name, aux])
         elif name == "audio":
-            pass
+            for att in self.dicc[name]:
+                aux[att] = attrs.get(att, "")
+            self.list.append([name, aux])
         elif name == "textstream":
-            pass
+            for att in self.dicc[name]:
+                aux[att] = attrs.get(att, "")
+            self.list.append([name, aux])
 
     def get_tags(self):
-        pass
+        return self.list
 
 if __name__ == "__main__":
     print("programa principal")
-    prueba = SmallSMILHandler()
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil'))
+    print(cHandler.get_tags())
